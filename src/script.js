9f9286cd94e339b1f298810f3b3652b4872c0683
claires-git <csplusjapan@gmail.com>
2025-01-07 22:29:24 +0900

    Added api for temperature

--- src/script.js
+++ src/script.js
@@ -1,9 +1,26 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let = cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "51o55c34aafbaa304dt54a5c8c2f6a03";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function searchHandler(event) {
   event.preventDefault();
   let = searchInput = document.querySelector("#search-form-input");
-  let = cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchHandler);
+
+searchCity("Tokyo");
